96df02ea061acdbc2211f7cda8a0e233d4c44b71
Elexya <elexyatama@gmail.com>
2023-12-15 18:38:13 +0700

    added comment

--- medical-checkup.go
+++ medical-checkup.go
@@ -22,83 +22,83 @@ type info_pack struct {
 	category string
 }
 
-type info_mcu struct {
+type info_mcu struct { //mcu = medical check up
 	id      string
 	price   float64
-	period  string
+	period  string //periode, waktu mcu
 	patient info_patient
 	pack    info_pack
 }
 
 type patient_tab struct {
 	data [nmax_patient]info_patient
-	n    int
+	n    int //buat ngitung ada brp data dalam list/array
 }
 
 type pack_tab struct {
 	data [nmax_pack]info_pack
-	n    int
+	n    int //buat ngitung ada brp data dalam list/array
 }
 
 type mcu_tab struct {
 	data [nmax_patient]info_mcu
-	n    int
+	n    int //buat ngitung ada brp data dalam list/array
 }
 
 func main() {
-	fmt.Print("yes")
+	fmt.Print("yes") //blm selesai
 }
 
-func add_patient(patients *patient_tab, x info_patient) {
-	patients.data[patients.n] = x
-	patients.n++
+func add_patient(patients *patient_tab, x info_patient) { //buat nambahin data pasien
+	patients.data[patients.n] = x //masukin nilai x, yang nanti berupa input dari func main
+	patients.n++                  //tiap func add ini dipanggil, variabel n harus ditambah buat track brp banyk data dalam array
 }
 
-func add_pack(packs *pack_tab, x info_pack) {
+func add_pack(packs *pack_tab, x info_pack) { // sistem nya sama kae add_patient, cuma ini buat pack
 	packs.data[packs.n] = x
 	packs.n++
 }
 
-func add_mcu(mcues *mcu_tab, x info_mcu) {
+func add_mcu(mcues *mcu_tab, x info_mcu) { // sistem nya sama kae add_patient, cuma ini buat mcu
 	mcues.data[mcues.n] = x
 	mcues.n++
 }
 
 func remove_patient(patients *patient_tab, x int) {
-	for i := x; i < patients.n-1; i++ {
-		patients.data[i] = patients.data[i+1]
-	}
-	patients.n--
+	for i := x; i < patients.n-1; i++ { /* logika nya ini misal kita ada array 1 2 3 4 5, kalo mau hapus angka 3 */
+		patients.data[i] = patients.data[i+1] /*  harus geser stu satu sampe ujung, biar si angka 3 itu ke tumpuk dan nilainya ilang*/
+	} /*  awalnya gni 1 2 3 4 5, trus nanti gr2 geser jadi 1 2 4 5 0*/
+	patients.n-- /*  angka kanannya geser trus ke kiri mulai dari angka ke 3*/
 }
 
-func remove_pack(packs *pack_tab, x int) {
+func remove_pack(packs *pack_tab, x int) { //konsepnya sama kae remove_patient
 	for i := x; i < packs.n-1; i++ {
 		packs.data[i] = packs.data[i+1]
 	}
 	packs.n--
 }
 
-func remove_mcu(mcues *mcu_tab, x int) {
+func remove_mcu(mcues *mcu_tab, x int) { //konsepnya sama kae remove_patient
 	for i := x; i < mcues.n-1; i++ {
 		mcues.data[i] = mcues.data[i+1]
 	}
 	mcues.n--
 }
 
-func search_patient_from_pack(mcues mcu_tab, x string) {
+func search_patient_from_pack(mcues mcu_tab, x string) { // ini buat nampilin semua pasien dg pack tertentu, x itu parameter pack yang ingin diprint semua pasien terkaitnya
 	var count int = 1
-	for i := 0; i < mcues.n; i++ {
-		if mcues.data[i].pack.id == x {
+	for i := 0; i < mcues.n; i++ { // ini ngecek dari array index 0 sampe mcues.n, mcues.n ini tu ukuran array
+		if mcues.data[i].pack.id == x { // ini ngecek kalo misalnya data.pack.id sama kae apa yang kita cari, nanti diprint detailnya dibawah
 			fmt.Printf("%d. %s, %s\n", count, mcues.data[i].patient.name, mcues.data[i].patient.id)
 			count++
 		}
 	}
-	if count == 1 {
+	if count == 1 { //ini kondisi kalo ngga ada pasien dg pack yg pengen kita cari, indikasinya count bernilai 1
 		fmt.Print("No patient with that spesific pack listed")
 	}
 }
 
-func search_patient_from_period(mcues mcu_tab, x string) { //sequential search
+func search_patient_from_period(mcues mcu_tab, x string) { //sequential search ada disini
 	var count int = 1
 	for i := 0; i < mcues.n; i++ {
 		if mcues.data[i].period == x {
@@ -111,7 +111,7 @@ func search_patient_from_period(mcues mcu_tab, x string) { //sequential search
 	}
 }
 
-func search_patient(patients patient_tab, x string) { //binary search
+func search_patient(patients patient_tab, x string) { //binary search ada disini
 	sort_patient_id(&patients)
 	low, high := 0, patients.n-1
 	found := -1
@@ -130,14 +130,14 @@ func search_patient(patients patient_tab, x string) { //binary search
 		}
 	}
 
-	if found != -1 {
+	if found != -1 { //found kalo ngga -1 artinya data nya ditemuin, kalo ditemuin nanti langsung print detailnya
 		print_patient_detail(patients.data[found])
-	} else {
+	} else { //klo ga nemu pass error msg
 		fmt.Printf("Patient with ID %s not found\n", x)
 	}
 }
 
-func sort_period(mcues *mcu_tab) { //selection sort
+func sort_period(mcues *mcu_tab) { //selection sort ada disini
 	for i := 0; i < mcues.n-1; i++ {
 		minIndex := i
 		for j := i + 1; j < mcues.n; j++ {
@@ -149,7 +149,7 @@ func sort_period(mcues *mcu_tab) { //selection sort
 	}
 }
 
-func sort_pack(mcues *mcu_tab) { //selection sort
+func sort_pack(mcues *mcu_tab) { //selection sort ada disini
 	for i := 0; i < mcues.n-1; i++ {
 		minIndex := i
 		for j := i + 1; j < mcues.n; j++ {
@@ -161,7 +161,7 @@ func sort_pack(mcues *mcu_tab) { //selection sort
 	}
 }
 
-func sort_patient_id(patients *patient_tab) { //insertion sort
+func sort_patient_id(patients *patient_tab) { //insertion sort ada disini
 	for i := 1; i < patients.n; i++ {
 		key := patients.data[i]
 		j := i - 1
@@ -174,7 +174,9 @@ func sort_patient_id(patients *patient_tab) { //insertion sort
 	}
 }
 
-func print_all_patient(patients patient_tab) {
+// func2 dibawah ini algoritma biasa buat ngeprint, sesuai dengan judul func nya
+
+func print_all_patient(patients patient_tab) { // buat print semua list pasien
 	fmt.Println("Patient Data:")
 	for i := 0; i < patients.n; i++ {
 		fmt.Printf("Name: %s\n", patients.data[i].name)
@@ -186,7 +188,7 @@ func print_all_patient(patients patient_tab) {
 	}
 }
 
-func print_all_pack(packs pack_tab) {
+func print_all_pack(packs pack_tab) { // buat prin semua list pack
 	fmt.Println("Pack Data:")
 	for i := 0; i < packs.n; i++ {
 		fmt.Printf("Name: %s\n", packs.data[i].name)
@@ -196,7 +198,7 @@ func print_all_pack(packs pack_tab) {
 	}
 }
 
-func print_all_mcu(mcues mcu_tab) {
+func print_all_mcu(mcues mcu_tab) { // buat print semua list mcu
 	fmt.Println("MCU Data:")
 	for i := 0; i < mcues.n; i++ {
 		fmt.Printf("ID: %s\n", mcues.data[i].id)
@@ -208,7 +210,7 @@ func print_all_mcu(mcues mcu_tab) {
 	}
 }
 
-func print_patient_detail(x info_patient) {
+func print_patient_detail(x info_patient) { // buat print detail satu pasien
 	fmt.Println("Patient Details:")
 	fmt.Printf("Name: %s\n", x.name)
 	fmt.Printf("ID: %s\n", x.id)
