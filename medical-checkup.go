c678be75c4950e19c05ac638eeede2a481c1e5d2
Elexya <elexyatama@gmail.com>
2023-12-15 16:07:44 +0700

    add and remove algo completed

--- medical-checkup.go
+++ medical-checkup.go
@@ -41,7 +41,7 @@ type pack_tab struct {
 }
 
 type mcu_tab struct {
-	data [nmax_patient]info_patient
+	data [nmax_patient]info_mcu
 	n    int
 }
 
@@ -50,27 +50,39 @@ func main() {
 }
 
 func add_patient(patient_list *patient_tab, x info_patient) {
-
+	patient_list.data[patient_list.n] = x
+	patient_list.n++
 }
 
 func add_pack(pack_list *pack_tab, x info_pack) {
-
+	pack_list.data[pack_list.n] = x
+	pack_list.n++
 }
 
 func add_mcu(mcu_list *mcu_tab, x info_mcu) {
-
+	mcu_list.data[mcu_list.n] = x
+	mcu_list.n++
 }
 
 func remove_patient(patient_list *patient_tab, x int) {
-
+	for i := x; i < patient_list.n-1; i++ {
+		patient_list.data[i] = patient_list.data[i+1]
+	}
+	patient_list.n--
 }
 
 func remove_pack(pack_list *pack_tab, x int) {
-
+	for i := x; i < pack_list.n-1; i++ {
+		pack_list.data[i] = pack_list.data[i+1]
+	}
+	pack_list.n--
 }
 
 func remove_mcu(mcu_list *mcu_tab, x int) {
-
+	for i := x; i < mcu_list.n-1; i++ {
+		mcu_list.data[i] = mcu_list.data[i+1]
+	}
+	mcu_list.n--
 }
 
 func search_patient_from_pack(mcu_list mcu_tab, x string) {
